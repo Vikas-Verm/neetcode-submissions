fa174ca60e8c9c612bcfae5a328fd81e9eab957a
Vikas-Verm <103033546+Vikas-Verm@users.noreply.github.com>
2026-06-09 14:11:12 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let str=s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+        let left=0
+        let right=str.length-1
+        while(left<right){
+            if(str[left]!=str[right]){
+                return false
+            }
+            left++
+            right--
+        }
+        return true
+    }
+}
